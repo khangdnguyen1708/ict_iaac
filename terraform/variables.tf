df6d85fce0de0687a5f43786be57cfad46dc8bf2
khangdnguyen1708 <kdnguyen1708@gmail.com>
2024-06-14 10:49:57 +0700

    imac

---
+++ terraform/variables.tf
@@ -0,0 +1,67 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc-name" {
+  description = "VPC Name for our Jumphost server"
+  type        = string
+  default     = "khang-Jumphost-vpc"
+}
+
+variable "igw-name" {
+  description = "Internet Gate Way Name for our Jumphost server"
+  type        = string
+  default     = "khang-Jumphost-igw"
+}
+
+variable "subnet-name" {
+  description = "Subnet Name for our Jumphost server"
+  type        = string
+  default     = "khang-Jumphost-subnet"
+}
+
+variable "rt-name" {
+  description = "Route Table Name for our Jumphost server"
+  type        = string
+  default     = "khang-Jumphost-rt"
+}
+
+variable "sg-name" {
+  description = "Security Group for our Jumphost server"
+  type        = string
+  default     = "khang-Jumphost-sg"
+}
+
+
+variable "iam-role" {
+  description = "IAM Role for the Jumphost Server"
+  type        = string
+  default     = "khang-Jumphost-iam-role"
+}
+
+variable "ami_id" {
+  description = "khang-AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-04b70fa74e45c3917" // Replace with the latest AMI ID for your region
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t4g.large"
+}
+
+variable "key_name" {
+  description = "EC2 keypair"
+  type        = string
+  default     = "khang-gitopskey"
+}
+
+variable "instance_name" {
+  description = "EC2 Instance name for the jumphost server"
+  type        = string
+  default     = "khang-Jumphost-server"
+}
+#
